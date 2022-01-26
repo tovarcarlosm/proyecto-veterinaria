3638e1ddb8dc22752fa6aaa1498de2276f43d63e
Carlos Tovar <kharlozm@gmail.com>
2022-01-26 18:33:03 -0500

    Commit inicial.

---
+++ src/app/modulo_admin/interfaces/Rol.Interface.ts
@@ -0,0 +1,6 @@
+export interface RolInterface {
+  _id: string;
+  name: string;
+  description: string;
+  permissions: string[];
+}
---
+++ src/app/modulo_tienda/interfaces/Product.Interface.ts
@@ -0,0 +1,10 @@
+export interface ProductInterface {
+  _id: string;
+  name: string;
+  code: string;
+  description: string;
+  purchasePrice: string;
+  salePrice: string;
+  quantity: number;
+  stock: number;
+}
