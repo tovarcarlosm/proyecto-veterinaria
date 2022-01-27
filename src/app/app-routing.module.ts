de22e2ced55df665ffea850681c0ae05bab963ae
Carlos Tovar <kharlozm@gmail.com>
2022-01-26 21:41:29 -0500

    Changes by Juanes BC

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashboardAdminComponent} from "./modulo_admin/components/dashboard-admin/dashboard-admin.component";
+import {DashboardTiendaComponent} from "./modulo_tienda/components/dashboard-tienda/dashboard-tienda.component";
+import {DashboardUsuarioComponent} from "./modulo_usuario/components/dashboard-usuario/dashboard-usuario.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard-admin' , component: DashboardAdminComponent},
+  { path: 'dashboard-tienda' , component: DashboardTiendaComponent},
+  { path: 'dashboard-usuario', component: DashboardUsuarioComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'dashboard-admin'}]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
